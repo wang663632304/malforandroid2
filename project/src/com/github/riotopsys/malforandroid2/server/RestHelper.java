b67136b4c91c7f92ec3efb3eb9d34d026875661f
C. Adam Fitzgerald <courtney.adam.fitzgerald@gmail.com>
2013-04-19 00:07:08 -0500

    adding licence notice

--- project/src/com/github/riotopsys/malforandroid2/server/RestHelper.java
+++ project/src/com/github/riotopsys/malforandroid2/server/RestHelper.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2012 C. A. Fitzgerald
+ * Copyright 2013 C. A. Fitzgerald
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
